2b1f60f5fa3cc1ae736b64307f3b8f0ef37c76b0
nithin-gangadharan-rangaraj <73269286+nithin-gangadharan-rangaraj@users.noreply.github.com>
2024-06-12 15:55:42 +1000

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.set_page_config(page_title="Internship")
+st.subheader("Web Developer Intern", divider = 'red')
+
+def display_job():
+  st.write('Welcome')
+
+if __name__ == "__main__":
+  display_job()
